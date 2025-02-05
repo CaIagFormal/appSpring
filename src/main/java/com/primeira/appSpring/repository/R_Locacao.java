9f361e2dce0a82193f5b036876a1d2366e319e15
Aluno <a@a>
2025-02-05 17:21:01 -0300

    optimizar query das locacao

--- src/main/java/com/primeira/appSpring/repository/R_Locacao.java
+++ src/main/java/com/primeira/appSpring/repository/R_Locacao.java
@@ -16,37 +16,53 @@ public interface R_Locacao extends JpaRepository<M_Locacao, Long> {
     @Query(value = "SELECT * FROM LOCACAO WHERE ID_USUARIO = :USUARIO",nativeQuery = true)
     List<M_Locacao> getLocacaoByUsuario(@Param("USUARIO") Long usuario);
 
-    @Query(value = "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
-            "CASE (L.CHECKOUT-L.CHECKIN) " +
-            "WHEN 0 THEN 1 ELSE (L.CHECKOUT-L.CHECKIN) END AS DIARIAS, " +
-            "CASE WHEN (SUM(C.PRECO)) IS NULL THEN 0 ELSE (SUM(C.PRECO)) END AS CONSUMOS " +
-            "FROM LOCACAO L " +
-            "JOIN QUARTO Q ON Q.ID=L.ID_QUARTO " +
+    @Query(value = "WITH CALC AS (SELECT (L.CHECKOUT-L.CHECKIN) AS DIARIAS, " +
+            "SUM(C.PRECO) AS CON FROM LOCACAO L " +
             "LEFT JOIN CONSUMO C ON C.ID_LOCACAO = L.ID " +
             "WHERE L.ID_USUARIO = :USUARIO AND NOW() >= L.CHECKOUT " +
-            "GROUP BY L.ID,Q.NUM ORDER BY L.CHECKIN DESC,L.CHECKOUT DESC",nativeQuery = true)
+            "GROUP BY L.ID) " +
+            "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
+            "CASE (CALC.DIARIAS) " +
+            "WHEN 0 THEN 1 ELSE (CALC.DIARIAS) END AS DIARIAS, " +
+            "CASE WHEN (CALC.CON) IS NULL THEN 0 ELSE (CALC.CON) END AS CONSUMOS " +
+            "FROM CALC " +
+            "JOIN LOCACAO L ON L.ID_USUARIO = :USUARIO " +
+            "JOIN QUARTO Q ON Q.ID=L.ID_QUARTO " +
+            "WHERE NOW() >= L.CHECKOUT " +
+            "ORDER BY L.CHECKIN DESC,L.CHECKOUT DESC",nativeQuery = true)
     List<M_ViewLocacao> getLocacaoCompleta(@Param("USUARIO") Long usuario);
 
-    @Query(value = "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
-            "CASE (L.CHECKOUT-L.CHECKIN) " +
-            "WHEN 0 THEN 1 ELSE (L.CHECKOUT-L.CHECKIN) END AS DIARIAS,0 AS CONSUMOS " +
+    @Query(value = "WITH CALC AS (SELECT (L.CHECKOUT-L.CHECKIN) AS DIARIAS " +
             "FROM LOCACAO L " +
+            "LEFT JOIN CONSUMO C ON C.ID_LOCACAO = L.ID " +
+            "WHERE L.ID_USUARIO = :USUARIO AND NOW() < L.CHECKIN) " +
+            "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
+            "CASE (CALC.DIARIAS) " +
+            "WHEN 0 THEN 1 ELSE (CALC.DIARIAS) END AS DIARIAS, " +
+            "0 AS CONSUMOS " +
+            "FROM CALC " +
+            "JOIN LOCACAO L ON L.ID_USUARIO = :USUARIO " +
             "JOIN QUARTO Q ON Q.ID=L.ID_QUARTO " +
-            "WHERE L.ID_USUARIO = :USUARIO AND NOW() < L.CHECKIN " +
+            "WHERE NOW() < L.CHECKIN " +
             "ORDER BY L.CHECKIN DESC,L.CHECKOUT DESC",nativeQuery = true)
     List<M_ViewLocacao> getLocacaoEmReserva(@Param("USUARIO") Long usuario);
 
-    @Query(value = "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
-            "CASE (L.CHECKOUT-L.CHECKIN) " +
-            "WHEN 0 THEN 1 ELSE (L.CHECKOUT-L.CHECKIN) END AS DIARIAS, " +
-            "CASE WHEN (SUM(C.PRECO)) IS NULL THEN 0 ELSE (SUM(C.PRECO)) END AS CONSUMOS " +
-            "FROM LOCACAO L " +
-            "JOIN QUARTO Q ON Q.ID=L.ID_QUARTO " +
+
+    @Query(value = "WITH CALC AS (SELECT (L.CHECKOUT-L.CHECKIN) AS DIARIAS, " +
+            "SUM(C.PRECO) AS CON FROM LOCACAO L " +
             "LEFT JOIN CONSUMO C ON C.ID_LOCACAO = L.ID " +
             "WHERE L.ID_USUARIO = :USUARIO AND NOW() BETWEEN L.CHECKIN AND L.CHECKOUT " +
-            "GROUP BY L.ID,Q.NUM ORDER BY L.CHECKIN DESC,L.CHECKOUT DESC",nativeQuery = true)
+            "GROUP BY L.ID) " +
+            "SELECT L.ID,Q.NUM,L.PRECO,L.SENHA,L.CHECKIN,L.CHECKOUT, " +
+            "CASE (CALC.DIARIAS) " +
+            "WHEN 0 THEN 1 ELSE (CALC.DIARIAS) END AS DIARIAS, " +
+            "CASE WHEN (CALC.CON) IS NULL THEN 0 ELSE (CALC.CON) END AS CONSUMOS " +
+            "FROM CALC " +
+            "JOIN LOCACAO L ON L.ID_USUARIO = :USUARIO " +
+            "JOIN QUARTO Q ON Q.ID=L.ID_QUARTO " +
+            "WHERE NOW() BETWEEN L.CHECKIN AND L.CHECKOUT " +
+            "ORDER BY L.CHECKIN DESC,L.CHECKOUT DESC",nativeQuery = true)
     List<M_ViewLocacao> getLocacaoEmCurso(@Param("USUARIO") Long usuario);
-
     @Query(value = "SELECT * FROM LOCACAO WHERE ID = :ID AND ID_USUARIO = :USUARIO LIMIT 1",nativeQuery = true)
     M_Locacao getLocacaoByIdAndUser(@Param("ID") Long id,@Param("USUARIO") Long usuario);
 }
